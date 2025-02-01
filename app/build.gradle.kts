05f0f2f6a78f85035f25018ee1b32e242e4f1cc6
NeetCheat <susandsouza0123@gmail.com>
2025-01-31 23:37:40 -0800

    chore(build): Updated version name and version code for API integration, UI fixes, and crash resolution
    -Updated version name from 1.0.0 to 1.1.4 in build.gradle.
    -Incremented version code from 1 to 2.
    -Preparing the app for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         minSdk = 26
         targetSdk = 35
 
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.4"
 
         applicationId = "com.glassdoor.intern"
 
