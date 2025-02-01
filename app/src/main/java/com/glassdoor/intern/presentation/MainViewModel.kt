8752c5cdc4c3bb4a85691982a3c1bc0b0701e851
NeetCheat <susandsouza0123@gmail.com>
2025-01-31 17:00:57 -0800

    fix: The previously loaded list remains on the screen after the error appears
    - Updated updateUiState() in MainViewModel.kt to prevent clearing the list when API error occurs

--- app/src/main/java/com/glassdoor/intern/presentation/MainViewModel.kt
+++ app/src/main/java/com/glassdoor/intern/presentation/MainViewModel.kt
@@ -107,7 +107,7 @@ internal class MainViewModel @Inject constructor(
         is UpdateErrorMessageState -> with(partialState) {
             previousUiState.copy(
                 errorMessage = errorMessage,
-                items = if (errorMessage.isNullOrEmpty()) previousUiState.items else emptyList(),
+                items = /*if (errorMessage.isNullOrEmpty())*/ previousUiState.items //else emptyList(),
             )
         }
 
